6848f32cec90d73765daf53e5744bf9293174f2e
Alessio Bianchi <venator85@gmail.com>
2018-07-01 10:22:59 +0200

    gradle update

--- demo/src/main/java/eu/alessiobianchi/floatlabel/MainActivity.java
+++ demo/src/main/java/eu/alessiobianchi/floatlabel/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		FloatLabel t = ((FloatLabel) findViewById(R.id.txt1));
+		FloatLabel t = findViewById(R.id.txt1);
 		t.setHintTextColor(Color.BLUE);
 		t.reloadEditTextHintTextColor();
 
